9f77a74d540cbaaeb2d6af76dc7c41283ffaa663
Manu <at0mikmychaine@gmail.com>
2022-04-25 21:37:39 +0200

    Tentative de fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -112,11 +112,11 @@ class ProfileController extends Controller
                 : $_SERVER['REMOTE_ADDR']);*/
  
         //whether ip is from the share internet  
-            if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+            if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
                     $ip = $_SERVER['HTTP_CLIENT_IP'];  
             }  
         //whether ip is from the proxy  
-        elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
                     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
             }  
         //whether ip is from the remote address  
@@ -124,8 +124,7 @@ class ProfileController extends Controller
                     $ip = $_SERVER['REMOTE_ADDR'];  
             }  
                 
-        
-        $ip = getIPAddress();  
+         
         dd($ip);  
 
         $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
