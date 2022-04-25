8ed63fcc5bdd4a8c6fa301f5e2074e9ffa8fbfb8
Manu <at0mikmychaine@gmail.com>
2022-04-25 21:34:07 +0200

    Tentative de fix IP

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -110,6 +110,24 @@ class ProfileController extends Controller
             : ($_SERVER['HTTP_X_FORWARDED_FOR'] 
                 ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
                 : $_SERVER['REMOTE_ADDR']);*/
+        function getIPAddress() {  
+            //whether ip is from the share internet  
+                if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+                        $ip = $_SERVER['HTTP_CLIENT_IP'];  
+                }  
+            //whether ip is from the proxy  
+            elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+                }  
+        //whether ip is from the remote address  
+            else{  
+                        $ip = $_SERVER['REMOTE_ADDR'];  
+                }  
+                return $ip;  
+        }  
+        $ip = getIPAddress();  
+        dd($ip);  
+
         $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
 
         Auth()->user()->update([
