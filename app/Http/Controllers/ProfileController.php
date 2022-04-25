5680b4544f3512fc563036996f4646b567c02c2e
Manu <at0mikmychaine@gmail.com>
2022-04-25 21:36:32 +0200

    Tentative Fix IP

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -110,21 +110,21 @@ class ProfileController extends Controller
             : ($_SERVER['HTTP_X_FORWARDED_FOR'] 
                 ? $_SERVER['HTTP_X_FORWARDED_FOR'] 
                 : $_SERVER['REMOTE_ADDR']);*/
-        function getIPAddress() {  
-            //whether ip is from the share internet  
-                if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
-                        $ip = $_SERVER['HTTP_CLIENT_IP'];  
-                }  
-            //whether ip is from the proxy  
-            elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-                }  
+ 
+        //whether ip is from the share internet  
+            if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];  
+            }  
+        //whether ip is from the proxy  
+        elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+            }  
         //whether ip is from the remote address  
-            else{  
-                        $ip = $_SERVER['REMOTE_ADDR'];  
-                }  
-                return $ip;  
-        }  
+        else{  
+                    $ip = $_SERVER['REMOTE_ADDR'];  
+            }  
+                
+        
         $ip = getIPAddress();  
         dd($ip);  
 
