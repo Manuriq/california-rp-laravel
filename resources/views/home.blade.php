d41bb1bcde380a55b027e24a0071984251da4c82
Manu <at0mikmychaine@gmail.com>
2022-05-14 23:19:45 +0200

    Ajout de GTA SA RAR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -153,7 +153,7 @@
         </div>
         <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
           <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+            <a href="{{ @asset('games/gta-san-andreas.rar') }}" class="flex flex-wrap no-underline hover:no-underline">
               <p class="w-full text-gray-600 text-xs md:text-sm px-6">
                 Etape n°1
               </p>
@@ -168,7 +168,7 @@
           <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
             <div class="flex items-center justify-center">
               <button class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                <a href="">Télécharger GTA San Andreas</a>
+                <a href="{{ @asset('games/gta-san-andreas.rar') }}">Télécharger GTA San Andreas</a>
               </button>
             </div>
           </div>
