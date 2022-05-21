8ae13cf1d8bd40416c0fd96f1e6c89ca8113bb59
Manu <at0mikmychaine@gmail.com>
2022-05-21 20:54:57 +0200

    Fix

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -29,7 +29,7 @@ class DiscordController extends Controller
         $user = Socialite::driver('discord')->user();
 
         $compte = Compte::query()->where('discord_id', $user->id)->get();
-        dd($compte);
+        dd($compte->id);
         if($compte->id != Auth()->user()->id){
 
             Session::flash('title', 'Errur !'); 
