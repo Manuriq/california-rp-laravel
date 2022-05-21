942c46a6c040abd1e2ae5a69cb5630586340eef5
Manu <at0mikmychaine@gmail.com>
2022-05-21 15:09:10 +0200

    Test

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -27,11 +27,11 @@ class DiscordController extends Controller
         $user = Socialite::driver('discord')->user();
 
         Auth()->user()->update([
-            'discord_id'=>$user->user->id,
-            'discord_name'=>$user->user->username,
-            'discord_disc'=>$user->user->discriminator,
-            'discord_email'=>$user->user->email,
-            'discord_verified'=>$user->user->verified
+            'discord_id'=>$user->id,
+            'discord_name'=>$user->user['username'],
+            'discord_disc'=>$user->user['discriminator'],
+            'discord_email'=>$user->user['email'],
+            'discord_verified'=>$user->user['verified']
         ]);
 
         return redirect()->back();
