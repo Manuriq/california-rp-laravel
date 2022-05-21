e492d0777ff8f238b17aa36304c560a6dd6e6fcb
Manu <at0mikmychaine@gmail.com>
2022-05-21 15:04:50 +0200

    Test Sync Discord

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -23,9 +23,17 @@ class DiscordController extends Controller
      */
     public function handleProviderCallback()
     {
-        //$user = Socialite::driver('discord')->stateless()->user();
+
         $user = Socialite::driver('discord')->user();
 
-        dd($user);
+        Auth()->user()->update([
+            'discord_id'=>$user->user->id,
+            'discord_name'=>$user->user->username,
+            'discord_disc'=>$user->user->discriminator,
+            'discord_email'=>$user->user->email,
+            'discord_verified'=>$user->user->verified
+        ]);
+
+        return redirect()->back();
     }
 }
