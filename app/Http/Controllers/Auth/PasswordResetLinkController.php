c3529cfccae0fa9d6bf7393967da4a8772342385
Manu <at0mikmychaine@gmail.com>
2022-05-24 23:32:40 +0200

    Fix STR

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 
 class PasswordResetLinkController extends Controller
 {
