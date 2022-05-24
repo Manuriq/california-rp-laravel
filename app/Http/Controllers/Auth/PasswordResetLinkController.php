2cd052def607d81785f38b34aca6ad0acc2805e8
Manu <at0mikmychaine@gmail.com>
2022-05-25 00:21:49 +0200

    test

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -38,10 +38,10 @@ class PasswordResetLinkController extends Controller
         ]);
 
         $token = Str::random(60);
-        $passwordReset = PasswordReset::updateOrCreate([   
-            ['email' => $request->email, 'token' => $token],
+        $passwordReset = PasswordReset::updateOrCreate(  
+            ['email' => $request->email],
             ['email' => $request->email, 'token' => $token]
-        ]);
+        );
 
         Mail::to($request->email)->send(new ResetPassword($token, $request->email));
 
